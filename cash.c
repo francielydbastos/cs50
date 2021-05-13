d94fce02007cbad510f9c8e6267165ac16fd3a04
francielydbastos <francielydbastos@users.noreply.github.com>
2021-05-13 22:19:41 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -11,15 +11,15 @@ int main(void)
         n = get_float("How much do you owe? $");
     }
     while (n < 0);
-    
+
     int change = round(n * 100);
 
     //calculate the minimum number of coins
     int quarter = change / 25;
     int dimes = (change % 25) / 10;
     int nickels = ((change % 25) % 10) / 5;
-    int pennies = (((change % 25)%10)%5);
-    
+    int pennies = (((change % 25) % 10) % 5);
+
     //print answer
     int total = quarter + dimes + nickels + pennies;
     printf("Change owed: %i\n", total);
